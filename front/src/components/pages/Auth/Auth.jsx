d5c4de1ade6d803b8acae85e94e1acb4463dd624
Vgyand <gyandzhunczevv@gmail.com>
2022-05-10 10:16:47 +0300

    signtoken

--- front/src/components/pages/Auth/Auth.jsx
+++ front/src/components/pages/Auth/Auth.jsx
@@ -1,21 +1,41 @@
+
 import Button from '../../common/ui/Button/Button'
 import React, { useState } from 'react'
 import Layout from '../../common/Layout'
 import Field from '../../common/ui/Field/Field'
 import style from './Auth.module.sass'
 import Alert from '../../common/ui/Alert/Alert'
+import { useMutation } from 'react-query'
+import { $api } from '../../../api/api'
+import Loader from '../../common/ui/Loader'
 
 const Auth = () => {
-    const [pass, setPass] = useState('')
     const [email, setEmail] = useState('')
+    const [password, setPass] = useState('')
+
     const [type, setType] = useState('auth')
+
+    const { mutate: register, isLoading, error } = useMutation('Registration', () =>
+        $api({
+            url: '/users',
+            type: 'POST',
+            body: { email, password },
+            auth: false,
+        }), {
+        onSuccess(data) {
+            localStorage.setItem('token', data.token)
+            console.log(data)
+        }
+    }
+    )
 
     const handleSubmit = (e) => {
         e.preventDefault()
         if (type === 'auth') {
+
             console.log('auth')
         } else {
-            console.log('reg')
+            register()
         }
 
     }
@@ -24,10 +44,11 @@ const Auth = () => {
         <Layout>
             <div className={style.wrapper}>
                 <h1 className={style.title}>Registration / Auth</h1>
-                {true && <Alert type={'error'} text={'suc'} />}
+                {error && <Alert type={'error'} text={error} />}
+                {isLoading && <Loader />}
                 <form onSubmit={handleSubmit}>
                     <Field type='email' placeholder={'Email'} value={email} onChange={e => setEmail(e.target.value)} />
-                    <Field type='password' placeholder={'Password'} value={pass} onChange={e => setPass(e.target.value)} />
+                    <Field type='password' placeholder={'Password'} value={password} onChange={e => setPass(e.target.value)} />
                     <div className={style.wrapper_btn}>
                         <Button text='Sign In' callback={() => setType('auth')} />
                         <Button text='Sign Up' callback={() => setType('reg')} />
